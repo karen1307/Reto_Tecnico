60be60839355881929f498e8ac83a9a75245b1d7
karen1307 <Karen_fonseca82151@elpoli.edu.co>
2024-09-30 15:45:49 -0500

    Se modifican mockeos del userinterfaces de HomeSanAngel

--- src/main/java/co/com/screenplay/project/userinterfaces/HomeSanAngel.java
+++ src/main/java/co/com/screenplay/project/userinterfaces/HomeSanAngel.java
@@ -6,13 +6,13 @@ public class HomeSanAngel {
 
     public static final Target PRODUCTO_UNO_FLORES_DESTELLOS =
             Target.the("Flores destellos").
-                    locatedBy("//body/div[@id='primary']/main[@id='main']/article[@id='post-4492']/div[1]/section[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[2]/a[2]/img[1]");
+                    locatedBy("(//h2[@class='woocommerce-loop-product__title' and contains(text(), 'Destellos')])[1]");
 
     public static final Target PRODUCTO_DOS_FLORES_PRESENTE =
             Target.the("Flores evora").
-                    locatedBy("//body/div[@id='primary']/main[@id='main']/article[@id='post-4492']/div[1]/section[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/a[1]/img[1]");
+                    locatedBy("//h2[@class='woocommerce-loop-product__title' and contains(text(), 'Évora – 24 rosas')]");
 
     public static final Target BOTON_HOME =
             Target.the("Ir al Home").
-                    locatedBy("//body[1]/div[1]/header[1]/div[2]/div[1]/a[1]/img[1]");
+                    locatedBy("(//a[@class='navmenu__logo-link'])[1]");
 }
